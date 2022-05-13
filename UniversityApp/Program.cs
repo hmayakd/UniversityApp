de17835a19d93ee41c212e81c765b91c06c59ff2
Hmayak Davtyan <davtyan.hmayak@gmail.com>
2022-05-13 19:36:04 +0400

    Fixed null exaption.

--- UniversityApp/Program.cs
+++ UniversityApp/Program.cs
@@ -16,24 +16,24 @@ namespace UniversityApp
             Student swappedStd = new Student();
             swappedStd = UniversityManager.SwapWithTeacher(student, teacher);
             swappedStd = UniversityManager.SwapWithGroup(swappedStd, group);
-            //StudentManager.Print(student);
-            //StudentManager.Print(swappedStd);
+            StudentManager.Print(student);
+            StudentManager.Print(swappedStd);
 
             Student[] swappedStds = new Student[students.Length];
             swappedStds = UniversityManager.SwapWithTeacher(students, teacher);
             swappedStds = UniversityManager.SwapWithGroup(swappedStds, group);
-            //StudentManager.Print(students);
-            //StudentManager.Print(swappedStds);
+            StudentManager.Print(students);
+            StudentManager.Print(swappedStds);
 
             Teacher swappedTch = new Teacher();
             swappedTch = UniversityManager.SwapWithStudents(teacher, students);
             swappedTch = UniversityManager.SwapWithGroup(swappedTch, group);
-            //TeacherManager.Print(teacher);
-            //TeacherManager.Print(swappedTch);
+            TeacherManager.Print(teacher);
+            TeacherManager.Print(swappedTch);
 
             Teacher[] swappedTchs = new Teacher[teachers.Length];
             swappedTchs = UniversityManager.SwapWithStudents(teachers, students);
-            swappedTchs = UniversityManager.SwapWithGroups(teachers, group);
+            swappedTchs = UniversityManager.SwapWithGroups(swappedTchs, group);
             TeacherManager.Print(teachers);
             TeacherManager.Print(swappedTchs);
         }
